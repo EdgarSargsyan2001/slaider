130585a27b8968d43d17c97bd80bbf8827edd238
EdgarSargsyan2001 <edgar.sargsyan.2001@mail.ru>
2022-03-14 15:08:50 +0400

    error

--- src/App.js
+++ src/App.js
@@ -7,9 +7,19 @@ function App() {
   const [showButtonNext,setShowButtonNext] = useState(true)
   const [showButtonPrev,setShowButtonPrev] = useState(false)
   const [flagInt,setFlagInt] = useState(true)
-
+  
   const translateLength = activeItemPosition * ( (window.screen.width > 500)?998:270 )
-
+  
+  const items = [
+    "https://artworld.ru/images/cms/content/catalog2/rodriguez_kartina_maslom_citrusovyj_fresh_apelsiny_jr190515.jpg",
+    "https://zvetnoe.ru/upload/images/blog/kartini/001.jpg",
+    "https://artworld.ru/images/cms/content/catalog2/vlodarchyk_kartina_maslom_derevo_zhelanij_sirenevyj_cvet_av200802.jpg",
+    "https://www.vinterier.ru/pictures/shop/v-sele-kartina-maslom-40x30.JPG",
+    "https://cdn1.ozone.ru/s3/multimedia-3/c1200/6000199911.jpg",
+    "https://molbert.com.ua/img/gallery/big/picture_8438_6064.webp",
+    "http://kraski-zhizni.com/wp-content/uploads/2020/06/IMG_4418.jpeg",
+  
+  ]
   
   function nextClick(){
     if(activeItemPosition < items.length - 1 ){
@@ -17,7 +27,7 @@ function App() {
       setShowButtonPrev(true)
       
   }
-  if(activeItemPosition == items.length-2){
+  if(activeItemPosition === items.length-2){
     setShowButtonNext(!showButtonNext)
   }
 }
@@ -33,8 +43,7 @@ function prevClick(){
   }
 
 }
-
-useEffect(()=>{
+function autoReplace(){
   let clearInt
 
   if(activeItemPosition < items.length - 1 && flagInt){
@@ -43,14 +52,14 @@ useEffect(()=>{
       setActiveItemPosition((prev)=>prev + 1)
       setShowButtonPrev(true)
       if(activeItemPosition === items.length - 2 ) setShowButtonNext(false)
-    },5000)
+    },1000)
 
   }else{
 
     clearInt = setTimeout(()=>{
       setActiveItemPosition((prev)=>prev - 1)
       setShowButtonNext(true)
-    },5000)
+    },1000)
 
     setFlagInt(false)
   }
@@ -58,23 +67,17 @@ useEffect(()=>{
     setFlagInt(true)
     setShowButtonPrev(false)
   }
+  return clearInt
+}
 
+useEffect(()=>{
+   let clearInt = autoReplace()
+ 
   return () => clearTimeout(clearInt)
     
-  
 })
 
 
-const items = [
-  "https://artworld.ru/images/cms/content/catalog2/rodriguez_kartina_maslom_citrusovyj_fresh_apelsiny_jr190515.jpg",
-  "https://zvetnoe.ru/upload/images/blog/kartini/001.jpg",
-  "https://artworld.ru/images/cms/content/catalog2/vlodarchyk_kartina_maslom_derevo_zhelanij_sirenevyj_cvet_av200802.jpg",
-  "https://www.vinterier.ru/pictures/shop/v-sele-kartina-maslom-40x30.JPG",
-  "https://cdn1.ozone.ru/s3/multimedia-3/c1200/6000199911.jpg",
-  "https://molbert.com.ua/img/gallery/big/picture_8438_6064.webp",
-  "http://kraski-zhizni.com/wp-content/uploads/2020/06/IMG_4418.jpeg",
-
-]
 
 
 
@@ -102,7 +105,7 @@ const items = [
                   {transform:` rotateY(0deg)`,boxShadow:'rgb(0 0 0 / 45%) 0px 19px 15px -2px' }
             
               }
-              ><img className='imges' src={item}/></li>
+              ><img className='imges' alt={item} src={item}/></li>
             
             )
           }
