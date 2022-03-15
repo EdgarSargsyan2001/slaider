e91217df0a1fe6980915c680aae10beb8f6211bf
EdgarSargsyan2001 <edgar.sargsyan.2001@mail.ru>
2022-03-16 00:49:15 +0400

    change images

--- src/App.js
+++ src/App.js
@@ -19,13 +19,19 @@ function App() {
 
 
   const items = [
-    "https://artworld.ru/images/cms/content/catalog2/rodriguez_kartina_maslom_citrusovyj_fresh_apelsiny_jr190515.jpg",
-    "https://zvetnoe.ru/upload/images/blog/kartini/001.jpg",
-    "https://artworld.ru/images/cms/content/catalog2/vlodarchyk_kartina_maslom_derevo_zhelanij_sirenevyj_cvet_av200802.jpg",
-    "https://www.vinterier.ru/pictures/shop/v-sele-kartina-maslom-40x30.JPG",
-    "https://cdn1.ozone.ru/s3/multimedia-3/c1200/6000199911.jpg",
-    "https://molbert.com.ua/img/gallery/big/picture_8438_6064.webp",
-    "http://kraski-zhizni.com/wp-content/uploads/2020/06/IMG_4418.jpeg",
+    
+    "https://i.pinimg.com/736x/51/8f/eb/518feb311dfc20fb22da411ef3e2a60a.jpg",
+    "https://cdn.pixabay.com/photo/2018/08/03/04/36/couple-3581038_960_720.jpg",
+    "https://img.artpal.com/699991/14-20-12-22-8-20-22m.jpg",
+    "https://i.pinimg.com/736x/a6/f5/1d/a6f51d9370e421cbf493a4c119890006.jpg",
+    "https://st3.depositphotos.com/1428083/12725/i/600/depositphotos_127251668-stock-photo-lovers-in-the-moonlight.jpg",
+    "https://www.hdimages.pics/images/quotes/english/general/romantic-hd-wallpapers-1080p-52650-310743-mobile.jpg",
+    "https://i.pinimg.com/736x/c6/66/cf/c666cf7c7197b29b8b28c89441146f4d.jpg",
+    // "https://cdn.pixabay.com/photo/2018/02/12/10/45/heart-3147976__340.jpg",
+    // "https://www.jagranimages.com/images/newimg/11122019/11_12_2019-love_19834716.jpg",
+    // "https://dradospiritualsolutions.com/images/blog/love-s.jpeg",
+    // "https://www.unigreet.com/wp-content/uploads/2020/02/Couple-love-dp.jpg",
+ 
   
   ]
   
