cfa0a8925de162104dae637ea5911d7e0e790392
EdgarSargsyan2001 <edgar.sargsyan.2001@mail.ru>
2022-03-16 22:53:06 +0400

    change style

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useState } from 'react';
+import { AiFillPlayCircle,AiFillPauseCircle } from "react-icons/ai"
 import './App.css';
 
 
@@ -9,9 +10,11 @@ function App() {
   const [showButtonNext,setShowButtonNext] = useState(true)
   const [showButtonPrev,setShowButtonPrev] = useState(false)
   const [flagInt,setFlagInt] = useState(true)
-  const [autoRep,setAutoRep] = useState(true)
+  const [autoRep,setAutoRep] = useState(false)
+  const [flag,setflag] = useState(false)
   const [size,setZise] = useState(-0.4)
   const audioRef = useRef()
+  const firstDiv = useRef()
 
   
   const translateLength = activeItemPosition * ( (window.screen.width > 500)?1023:295 )
@@ -34,11 +37,8 @@ function App() {
   
   ]
   
-
   function nextClick(){
-    
-    // console.log(audioRef.current.style.display="none")
-    audioRef.current.play()
+  
     if(activeItemPosition < items.length - 1 ){
       setActiveItemPosition((prev)=>prev + 1)
       setShowButtonPrev(true)
@@ -52,7 +52,6 @@ function App() {
 
 function prevClick(){
   
-  // console.log(audioRef.current.style.display="inline")
   if(activeItemPosition > 0 ){
     setActiveItemPosition((prev)=>prev - 1)
     setShowButtonNext(true)
@@ -65,33 +64,33 @@ function prevClick(){
 }
 function autoReplace(){
   if(autoRep){
-  let clearInt
+    let clearInt
 
-  if(activeItemPosition < items.length - 1 && flagInt){
+    if(activeItemPosition < items.length - 1 && flagInt){
 
-    clearInt = setTimeout(()=>{
-      setActiveItemPosition((prev)=>prev + 1)
-      setShowButtonPrev(true)
-      setZise(-0.4)
-      if(activeItemPosition === items.length - 2 ) setShowButtonNext(false)
-    },3500)
+      clearInt = setTimeout(()=>{
+        setActiveItemPosition((prev)=>prev + 1)
+        setShowButtonPrev(true)
+        setZise(-0.4)
+        if(activeItemPosition === items.length - 2 ) setShowButtonNext(false)
+      },3500)
 
-  }else{
+    }else{
 
-    clearInt = setTimeout(()=>{
-      setActiveItemPosition((prev)=>prev - 1)
-      setShowButtonNext(true)
-      setZise(0.4)
-    },3500)
+      clearInt = setTimeout(()=>{
+        setActiveItemPosition((prev)=>prev - 1)
+        setShowButtonNext(true)
+        setZise(0.4)
+      },3500)
 
-    setFlagInt(false)
+      setFlagInt(false)
+    }
+    if(activeItemPosition === 0){
+      setFlagInt(true)
+      setShowButtonPrev(false)
+    }
+    return clearInt
   }
-  if(activeItemPosition === 0){
-    setFlagInt(true)
-    setShowButtonPrev(false)
-  }
-  return clearInt
-}
 }
 
 useEffect(()=>{
@@ -101,21 +100,31 @@ useEffect(()=>{
 
 })
 
+useEffect(()=>{
+
+  audioRef.current.style.display=`${autoRep?"block":"none"}`
+  if(autoRep){
+      audioRef.current.play()
+
+  }else{
+    audioRef.current.pause()
+  } 
+
+  
+},[autoRep])
+
   return (
     <div className="App">
-        
+      
       <div className='screen'
       
         style={
-               
                 {transform:` matrix3d(1, 0, ${size}, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)` }
-                   
-                 
-            
               }
-      >
+        >
         
         <ul className='ul'
+        
             style={{
               transform:`translateX(-${translateLength+activeItemPosition*2}px)`,
             
@@ -125,40 +134,55 @@ useEffect(()=>{
               <li 
                 key={Math.random()}
                 className='list'
-              //   style={
-              //     (activeItemPosition !== parseInt(items.length/2)) ? 
-              //     {transform:` rotateY(${ (activeItemPosition > parseInt(items.length/2)? 150-(activeItemPosition*12) : 100-(activeItemPosition*15))}deg)` }
-              //      : 
-              //     {transform:` rotateY(0deg)`,boxShadow:'rgb(0 0 0 / 45%) 0px 19px 15px -2px' }
-            
-              // }
-        
               ><img className='imges' alt={item} src={item}/></li>
             
             )
           }
         </ul>
+        
+        <div className='first' ref={firstDiv}>
+          {!flag &&
+            <button 
+              className='ButtonONOFF' 
+              onClick={ () => {
+                setAutoRep(!autoRep)
+                setflag(true)
+                firstDiv.current.style.opacity="0"
+  
+            }}>{AiFillPlayCircle({size:300,color:"rgba(28,47,106, 0.619)"})}</button>
+            
+          }
+        
+        </div>
+        
+
         {
           showButtonPrev && <button className='prev' onClick={prevClick} >{'<'}</button>
         }
         {
-          showButtonNext && <button className='next' onClick={nextClick} >{'>'}</button>
+          flag && showButtonNext && <button className='next' onClick={nextClick} >{'>'}</button>
         }
-
+        
       </div>
 
-      <button className='ButtonONOFF' onClick={ () => setAutoRep(!autoRep) }>{autoRep?"ON":"OFF"}</button>
+      {flag &&
+        <button 
+          className='ButtonONOFF' 
+          onClick={ () => setAutoRep(!autoRep) }>
+          {!autoRep?AiFillPlayCircle({size:45,color:"white"}):AiFillPauseCircle({size:45,color:"white"})}
+          
+        </button>
+      }
 
+      <audio 
+        src='../audio/fly.mp3'  
+        className='audio'
+        ref={audioRef} 
+        controls 
+        autoPlay
+        loop
 
-        <audio 
-          src='../audio/fly.mp3'  
-          className='audio'
-          ref={audioRef} 
-          controls 
-          autoPlay
-          loop
-  
-        />
+      />
           
     </div>
   );
